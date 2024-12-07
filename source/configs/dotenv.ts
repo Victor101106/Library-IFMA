a995be28ab101e21758d70007c57140041b556f9
Victor101106 <victorgabriel101106+github@gmail.com>
2024-12-07 18:33:20 -0300

    feat(env): google api credentials

--- source/configs/dotenv.ts
+++ source/configs/dotenv.ts
@@ -1,6 +1,9 @@
 import { EnvironmentSchema } from '../schemas'
 
 const environment = EnvironmentSchema.parse({
+    GOOGLE_CLIENT_SECRET_KEY: process.env.GOOGLE_CLIENT_SECRET_KEY,
+    GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     PORT: Number(process.env.PORT)
 })
 
--- source/schemas/configs/environment.schema.ts
+++ source/schemas/configs/environment.schema.ts
@@ -1,5 +1,8 @@
 import { z } from 'zod'
 
 export const EnvironmentSchema = z.object({
+    GOOGLE_CLIENT_SECRET_KEY: z.string(),
+    GOOGLE_REDIRECT_URI: z.string(),
+    GOOGLE_CLIENT_ID: z.string(),
     PORT: z.number()
 })
