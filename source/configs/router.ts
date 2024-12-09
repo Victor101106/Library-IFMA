e06442de4145686ff05fc9142f02722853f1d023
Victor101106 <victorgabriel101106+github@gmail.com>
2024-12-09 11:39:20 -0300

    feat: view routes

--- source/configs/router.ts
+++ source/configs/router.ts
@@ -4,11 +4,13 @@ import { join } from 'path'
 
 export default (instance: FastifyInstance): void => {
     
-    const routesFolderPath = join(__dirname, '../routes/')
+    const apiRoutesFolderPath = join(__dirname, '../routes/api/')
+    const viewRoutesFolderPath = join(__dirname, '../routes/views/')
 
-    if (!existsSync(routesFolderPath))
+    if (!existsSync(viewRoutesFolderPath) || !existsSync(apiRoutesFolderPath))
         return
-    
-    readdirSync(routesFolderPath).forEach(file => instance.register(require(`${routesFolderPath}/${file}`), { prefix: '/api' }))
+
+    readdirSync(apiRoutesFolderPath).forEach(file => instance.register(require(`${apiRoutesFolderPath}/${file}`), { prefix: '/api' }))
+    readdirSync(viewRoutesFolderPath).forEach(file => require(`${viewRoutesFolderPath}/${file}`)(instance))
 
 }
